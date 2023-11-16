4dc8ba1c9359e3c81d77cf744b113517552f1aa5
= <=>
2023-11-16 16:16:46 +0100

    creacion del programa en C#

--- Tarea2/Program.cs
+++ Tarea2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+         int number1, number2, total;
+        Console.WriteLine("Escribe un numero: ");
+        number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Escribe otro numero: ");
+        number2 = Convert.ToInt32(Console.ReadLine());
+        total = number1 + number2;
+        Console.WriteLine("La suma correcta és: " + total);
+        Console.ReadLine();
+    }
+}
